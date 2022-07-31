0cdb82cd08ceba34e1fa1e1cc50a03c3bb740ab1
RobertoFigueroa <fig18306@uvg.edu.gt>
2022-07-30 22:36:39 -0600

    [ADD] gui ready!

--- main.py
+++ main.py
@@ -1,13 +1,33 @@
-import sys
+import os
 from antlr4 import *
-from dist.YAPL import YAPLLexer
-from dist.YAPL import YAPLParser
-from dist.YAPL import YAPLVisitor
+from dist.YAPL.YAPLLexer import YAPLLexer
+from dist.YAPL.YAPLParser import YAPLParser
+from dist.YAPL.YAPLVisitor import YAPLVisitor
 
 #antlr4 -Dlanguage=Python3 YAPL/YAPL.g4 -visitor -o dist
 
 if __name__ == "__main__":
 
     dir_file = input("Ingrese el directorio en donde se encuentra el programa YAPL >>")
-    data = InputStream(dir_file)
+    # TODO: Check if file exists
+    _file = open(dir_file)
+    str_file = _file.read()
+    _file.close()
+    data = InputStream(str_file)
+    lexer = YAPLLexer(data)
+    stream = CommonTokenStream(lexer)
+    parser = YAPLParser(stream)
+    tree = parser.program()
+    
+    visitor = YAPLVisitor()
+    output = visitor.visit(tree)
+    
+    print(output)
+
+    # os.system("make compile")
+    # os.system("make run")
+    # os.system("make clean")
+    
+
+    
 
---
+++ makefile
@@ -0,0 +1,27 @@
+ANTLR = /usr/local/lib/antlr-4.10.1-complete.jar
+GRAMMAR_NAME = YAPL
+ANTLR_FLAGS = -no-visitor -no-listener
+OUT_DIR = jdist
+START_RULE = program
+CL_FILE = input/cool.cl
+GRUN_FLAGS = -gui -tokens
+INPUT_FILE = cool.cl
+
+%: all
+
+all: compile run
+
+compile:
+	@echo Generating $(GRAMMAR) Parser and Lex ...
+	java -Xmx500M -cp $(ANTLR) org.antlr.v4.Tool $(ANTLR_FLAGS) -o $(OUT_DIR) ./$(GRAMMAR_NAME)/$(GRAMMAR_NAME).g4
+	@echo Success!
+
+run:
+	@echo Showing parse tree ...
+	cd $(OUT_DIR)/$(GRAMMAR_NAME); javac ./*.java; \
+	java -Xmx500M -cp $(ANTLR):$(CLASSPATH) org.antlr.v4.gui.TestRig $(GRAMMAR_NAME) $(START_RULE) ../../input/$(INPUT_FILE) $(GRUN_FLAGS); \
+	echo Done!
+
+clean:
+	rm -r ./$(OUT_DIR)
+
